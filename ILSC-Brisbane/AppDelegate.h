0095c47c8040de37cac1b01ce55a2efc0e488be6
marslin <marslin@gmail.com>
2014-07-01 21:57:05 +1000

    Initial Commit

---
+++ ILSC-Brisbane/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ILSC-Brisbane
+//
+//  Created by 林 政龍 on 2014/7/1.
+//  Copyright (c) 2014年 marstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ILSC-Brisbane/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ILSC-Brisbane
+//
+//  Created by 林 政龍 on 2014/7/1.
+//  Copyright (c) 2014年 marstudio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

f652cbe9f298db11d21ab43b40eda10fe89fbebe
marslin <marslin@gmail.com>
2014-10-16 15:53:12 +1000

    Add information tableViewController to launch browser for links.

---
+++ ILSC-Brisbane/InformationTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  InformationTableViewController.h
+//  ILSC-Brisbane
+//
+//  Created by 林 政龍 on 2014/10/16.
+//  Copyright (c) 2014年 marstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InformationTableViewController : UITableViewController
+
+@end

04f4421d677cfa047631aa7173c3879c00bf3351
marslin <marslin@gmail.com>
2014-11-22 12:04:35 +1000

    Add youtube channels and blog link to "What's on" section.

--- ILSC-Brisbane/MenuViewController.m
+++ ILSC-Brisbane/MenuViewController.m
@@ -8,6 +8,10 @@
 
 #import "MenuViewController.h"
 
+#define ILSC_BNE_COMMUNITY_TV_URL @"https://m.youtube.com/channel/UCUmXuPKvW5uLoXtLl9a5tDQ/videos"
+#define ILSC_COMMUNITY_TV_URL @"https://m.youtube.com/user/ilscTV/videos"
+#define ILSC_BLOG_URL @"http://blog.ilsc.com"
+
 @interface MenuViewController ()
 
 @end
@@ -28,6 +32,25 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
+  UIApplication *applcation = [UIApplication sharedApplication];
+
+  if (indexPath.section == 0) {
+    switch (indexPath.row) {
+      case 0:
+        [applcation openURL:[NSURL URLWithString:ILSC_BNE_COMMUNITY_TV_URL]];
+        break;
+      case 1:
+        [applcation openURL:[NSURL URLWithString:ILSC_COMMUNITY_TV_URL]];
+        break;
+      case 2:
+        [applcation openURL:[NSURL URLWithString:ILSC_BLOG_URL]];
+        break;
+      default:
+        break;
+    }
+
+  }
+
   [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 

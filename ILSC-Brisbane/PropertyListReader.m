ab3486f305b36811e3a6581bdce7b20156674b79
marslin <marslin@gmail.com>
2015-02-16 18:55:22 +0800

    In order for one format specifier to work for both architectures, you must use a long specifier and cast the value to long.

--- ILSC-Brisbane/PropertyListReader.m
+++ ILSC-Brisbane/PropertyListReader.m
@@ -23,7 +23,7 @@
                                                 format:&format
                                                 error:&errorDesc];
     if (!properties) {
-        NSLog(@"Error reading plist: %@, format: %u", errorDesc, format);
+        NSLog(@"Error reading plist: %@, format: %lu", errorDesc, (unsigned long)format);
     }
 
     return properties;
